27c1b661447267bff161f53fe5f1a89edad17cc6
Piter Novian <piter@lipat.co.id>
2017-12-26 13:33:41 +0700

    update

--- api/modules/master/models/Product.php
+++ api/modules/master/models/Product.php
@@ -63,7 +63,7 @@ class Product extends \yii\db\ActiveRecord
     {
         return [
            // [['ACCESS_GROUP', 'STORE_ID', 'PRODUCT_ID', 'YEAR_AT', 'MONTH_AT'], 'required'],
-            [['STATUS', 'YEAR_AT', 'MONTH_AT','LINK_STS'], 'integer'],
+            [['STATUS', 'YEAR_AT', 'MONTH_AT','IMG_FILE'], 'integer'],
             [['PRODUCT_SIZE', 'STOCK_LEVEL'], 'number'],
             [['CREATE_AT', 'UPDATE_AT','CREATE_UUID','UPDATE_UUID','CURRENT_PRICE','INDUSTRY_ID','INDUSTRY_GRP_ID','CURRENT_STOCK'], 'safe'],
             [['DCRP_DETIL'], 'string'],
@@ -217,8 +217,8 @@ class Product extends \yii\db\ActiveRecord
 			'CURRENT_PROMO'=>function($model){
 				return $this->productPromoTbl;
 			},	
-			'LINK_STS'=>function($model){
-				return $model->LINK_STS;
+			'IMG_FILE'=>function($model){
+				return $model->IMG_FILE;
 			},	
         ];		
 	}
